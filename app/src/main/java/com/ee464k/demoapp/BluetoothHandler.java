ca73b07167b88363f6be32051665731268f0058f
Nick Requa <nick.requa@gmail.com>
2020-04-05 22:15:11 -0500

    Bluetooth communciation + Khalid's handler function to extract sensor data.

---
+++ app/src/main/java/com/ee464k/demoapp/BluetoothHandler.java
@@ -0,0 +1,38 @@
+package com.ee464k.demoapp;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Handler;
+import android.util.Log;
+
+import java.util.Set;
+import java.util.UUID;
+
+public class BluetoothHandler {
+    public String address;
+    public UUID MY_UUID = UUID.fromString("123");
+    String TAG = "BLHandler";
+    public BluetoothDevice hardwareBT = null;
+    public Set<BluetoothDevice> pairedDevices;
+
+    public BluetoothHandler(String address, Set<BluetoothDevice> pairedDevices){
+        this.address = address;
+        this.pairedDevices = pairedDevices;
+    }
+
+    public BluetoothDevice getDevice(){
+        BluetoothDevice desiredDevice = null;
+
+        for(BluetoothDevice device : pairedDevices){
+            Log.d(TAG, device.getName() + " : " + device.getAddress());
+            if(device.getAddress().equals(address)){
+                desiredDevice = device;
+            }
+        }
+
+        return desiredDevice;
+    }
+
+
+}
--- app/src/main/java/com/ee464k/demoapp/MainActivity.java
+++ app/src/main/java/com/ee464k/demoapp/MainActivity.java
@@ -3,9 +3,14 @@ package com.ee464k.demoapp;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -34,20 +39,94 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.Set;
+import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class MainActivity extends AppCompatActivity {
-    private static final Random RANDOM = new Random();
+
     private LineGraphSeries<DataPoint> series;
     private int lastX = 0;
     private double generatedIndex = 0;
+    private final String TAG = "MainActivity";
+    private final String address = "1";
+    public UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    public int DATA_IN = 1;
+    Handler bluetoothRead;
+    public StringBuilder dataString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create handler to update UI and store data
+        bluetoothRead = new Handler() {
+            public void handleMessage(Message msg){
+                if(msg.what == DATA_IN){
+                    // Supposed format of JSON string
+                    //{
+                    //  "TimeStamp": 442,
+                    //  "Sp02": 12,
+                    //  "PPG_HR": 24.36,
+                    //  "BodyTemperature": 24.35,
+                    //  "ECG": 509
+                    //}
+                    // JSON obect to JSON string format:
+                    String sensorData = "{\"TimeStamp\":12,\"Sp02\":34,\"PPG_HR\":56,\"BodyTemperature\":24.35,\"ECG\":509}";//(String) msg.obj;
+                    try {
+                        // Convert string to JSON Object
+                        JSONObject jObject = new JSONObject(sensorData);
+                        // Get seperate values from key/labels and put them into integers
+                        int timeStamp = jObject.getInt("TimeStamp");
+                        int Sp02 = jObject.getInt("Sp02");
+                        int PPG_HR = jObject.getInt("PPG_HR");
+                        int bodyTemperature = jObject.getInt("BodyTemperature");
+                        int ecg = jObject.getInt("ECG");
+                        // Here we would append each value to its appropriate string
+                        // ***TESTING***
+                        // SCREEN OUTPUT SHOULD BE: 123456
+                        dataString = new StringBuilder();
+                        dataString.append(Integer.toString(timeStamp));
+                        dataString.append(Integer.toString(Sp02));
+                        dataString.append(Integer.toString(PPG_HR));
+                        // ***TESTING***
+                        // TEST PASSED
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    // Parse dataString for our sensor data
+                   // message.setText(dataString);
+                    //dataString.delete(0, dataString.length());
+                }
+            }
+        };
+
+        // Set up bluetooth stuff
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetoothAdapter == null){
+            Log.e(TAG, "Bluetooth not supported");
+        }
+
+        if(!mBluetoothAdapter.isEnabled()){
+            Intent enableBTintent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBTintent, 1);
+        }
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+        // Get device we want
+        BluetoothHandler BTsetup = new BluetoothHandler(address, pairedDevices);
+        BluetoothDevice device = BTsetup.getDevice();
+
 
         // GraphView practice block
         GraphView graph = (GraphView) findViewById(R.id.graph);
@@ -64,17 +143,13 @@ public class MainActivity extends AppCompatActivity {
 
         // Set index
         final TextView indexSubmit = (TextView) findViewById(R.id.indexDisplay);
-        generatedIndex = ThreadLocalRandom.current().nextDouble(0,100);
-        generatedIndex = Math.round(generatedIndex);
         indexSubmit.setText(Double.toString(generatedIndex));
     }
 
     @Override
     protected  void onResume(){
         super.onResume();
-
         // Thread to append data
-
         new Thread(new Runnable() {
             @Override
             public void run(){
@@ -95,7 +170,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }).start();
-
 
     }
 
@@ -221,6 +295,107 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void addEntry(){
-        series.appendData(new DataPoint(lastX++, RANDOM.nextDouble() * 10d), true, 10);
+        series.appendData(new DataPoint(lastX++, 1), true, 10);
+    }
+
+    private class ConnectThread extends Thread{
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+
+        public ConnectThread(BluetoothDevice device){
+            BluetoothSocket tmp = null;
+            mmDevice = device;
+
+            try {
+                tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+                Log.e(TAG, "Socket creation failed", e);
+            }
+
+            mmSocket = tmp;
+        }
+
+        public void run() {
+
+            try {
+                mmSocket.connect();
+                Log.d(TAG, "Connected to socket");
+            } catch (IOException connectException){
+                // Can't connect, try to close socket
+                try {
+                    mmSocket.close();
+                } catch (IOException closeException){
+                    Log.e(TAG, "Coudn't close socket.", closeException);
+                }
+                // Should do something here, say restart app or loop a search?
+                Log.d(TAG, "Couldn't connect to socket");
+                return;
+            }
+
+            manageMyConnectedSocket(mmSocket);
+        }
+
+        public void cancel(){
+            try {
+                mmSocket.close();
+            } catch(IOException e){
+                Log.e(TAG, "Couldn't close socket", e);
+            }
+        }
+
+        private void manageMyConnectedSocket(BluetoothSocket socket){
+            ConnectedThread receiveThread = new ConnectedThread(socket);
+            receiveThread.start();
+        }
+    }
+
+    private class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+        private byte[] mmBuffer;
+
+        public ConnectedThread(BluetoothSocket socket){
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            try {
+                tmpIn = socket.getInputStream();
+            } catch (IOException e){
+                Log.e(TAG, "Error getting input stream.", e);
+            }
+
+            try {
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+                Log.e(TAG, "Error getting output stream.", e);
+            }
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run(){
+            mmBuffer = new byte[1024];
+            int numBytes;
+
+            while(true) {
+                try {
+                    numBytes = mmInStream.read(mmBuffer);
+                    String stringMessage = new String(mmBuffer, 0, numBytes);
+                    Message readMsg = bluetoothRead.obtainMessage(DATA_IN, numBytes, -1, stringMessage);
+                    bluetoothRead.sendMessage(readMsg);
+                } catch(IOException e){
+                    Log.d(TAG, "Input stream disconnected", e);
+                    break;
+                }
+            }
+        }
+
+
+
     }
 }
+
+
+
